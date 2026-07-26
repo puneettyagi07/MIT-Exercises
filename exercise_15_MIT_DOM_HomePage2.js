112ab44e911080be05a23c0e46c17ea6337ee9ca
Puneet Tyagi <152277556+puneettyagi07@users.noreply.github.com>
2026-07-26 22:07:26 +0530

    Add files via upload

--- exercise_15_MIT_DOM_HomePage2.js
+++ exercise_15_MIT_DOM_HomePage2.js
@@ -2,3 +2,16 @@ const body = document.body.children;
 console.log(body);
 
 const vibrantColorSection = document.getElementById(`vibrant_color`);
+vibrantColorSection.style.height = `fit-content`;
+vibrantColorSection.style.width = `100%`;
+vibrantColorSection.style.display = `flex`;
+vibrantColorSection.style.gap = `30px`;
+vibrantColorSection.style.alignItems = `center`;
+vibrantColorSection.style.padding = `100px 50px 50px 50px`;
+vibrantColorSection.style.backgroundImage = "linear-gradient(to top, rgb(255, 253, 208, 0.3), rgb(245, 245, 220, 0.3))";
+
+const section_1_leftSide = document.getElementsByClassName('section_1_left');
+section_1_leftSide.style.display = `flex`;
+section_1_leftSide.style.flexDirection = `column`;
+section_1_leftSide.style.gap = `120px`;
+
