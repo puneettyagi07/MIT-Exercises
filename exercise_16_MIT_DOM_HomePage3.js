d4ab748363aae9d753a959bbe6a63ccc627e2098
Puneet Tyagi <152277556+puneettyagi07@users.noreply.github.com>
2026-08-01 17:36:12 +0530

    Add files via upload

--- exercise_16_MIT_DOM_HomePage3.js
+++ exercise_16_MIT_DOM_HomePage3.js
@@ -1,26 +1,24 @@
+// Changing Cards & content and style using 
+
 const heading = document.getElementsByClassName('heading')[0];
-heading.outerHTML =  `<section class = "heading"><h1> Mind In Tech</h1> </section>`;
+heading.outerHTML = `<section class = "heading"><h1> Mind In Tech</h1> </section>`;
+// console.log(heading);
 
 const con_heading = document.getElementsByClassName('con-heading')[0];
-con_heading.outerHTML = `<section class = "con-heading><span class = "blueText">Updated</span> Curriculum </section>`;
-con_heading.children[0].style.display = 'flex';
-con_heading.children[0].style.alignItems = 'center';
-con_heading.style.color = 'blue';
+con_heading.outerHTML='<section class="con-heading"><h3><span class="blueText">Updated </span> Curriculam </h3></section>'
 // console.log(con_heading);
 
-// agr mai con_heading.children[0] laga tha to 1st ko replace kr k ye text aa tha h but second text nhi ja rha h but iss menthod se dono text ja rhe h but styling nhi ho rhi h
-
 const card = document.getElementsByClassName('card')[0];
-card.children[1].innerHTML = `<p> Full-Stack MERN & Next.js architectures.</p>`;
+card.children[1].innerHTML = `Full-Stack MERN & Next.js architectures.`;
 // console.log(card);  
-// inn sb me style kese lagane h vo btao
 
 const card2 = document.getElementsByClassName('card')[1];
-card2.children[1].innerHTML = `<p> Advanced Figma prototyping and design systems.</p>`;
+card2.children[1].innerHTML = `Advanced Figma prototyping and design systems.`;
+// console.log(card2);
 
 const card3 = document.getElementsByClassName('card')[2];
-card3.children[0].innerHTML = `<h5>Mind in Tech Junior</h5>`;
-card3.children[1].innerHTML = `<p>A specialized 6-month program building basic computer coordination and creativity.</p>`;
+card3.children[0].innerHTML = `Mind in Tech Junior`;
+card3.children[1].innerHTML = `A specialized 6-month program building basic computer coordination and creativity.`;
 card3.style.border = '2px dashed blue';
 card3.style.backgroundColor = 'rgb(131, 162, 190, 0.2)';
-// console.log(card3);
+console.log(card3);
